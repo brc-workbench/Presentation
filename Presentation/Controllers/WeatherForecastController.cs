d5c677f7cb6b8d70d799756f4692551e181e5f87
Brandon Clausing <b.clausing@avanade.com>
2020-02-18 14:43:08 +0000

    BFF now calls an external REST endpoint to get weather data.

--- Presentation/Controllers/WeatherForecastController.cs
+++ Presentation/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Presentation.ExternalRestApi;
 
 namespace Project.Presentation.Controllers
 {
@@ -24,16 +25,11 @@ namespace Project.Presentation.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<IEnumerable<WeatherForecast>> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            var weatherForecastApi = new WeatherForecastApi();
+
+            return await weatherForecastApi.GetAllForecasts();
         }
     }
 }
---
+++ Presentation/ExternalRestApi/WeatherForecastApi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Formatting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project.Presentation;
+
+namespace Presentation.ExternalRestApi
+{
+    public class WeatherForecastApi
+    {
+        private static readonly string baseUrl = "https://bclausingwb-api.azurewebsites.net/";
+        private static readonly string getAll = "weatherforecast";
+        private static HttpClient client;
+
+        public WeatherForecastApi()
+        {
+            client = new HttpClient();
+        }
+
+        public async Task<IEnumerable<WeatherForecast>> GetAllForecasts()
+        {
+            IEnumerable<WeatherForecast> result = null;
+
+            using (HttpResponseMessage response = await client.GetAsync(baseUrl + getAll))
+            {
+                response.EnsureSuccessStatusCode();
+                result = await response.Content?.ReadAsAsync<IEnumerable<WeatherForecast>>();
+            }
+
+            return result ?? new List<WeatherForecast>();
+        }
+    }
+}
